9098af4eb6018a171c34e27cef68166d551204a3
Jingfei Yao <sophiayao1990@gmail.com>
2022-09-27 10:50:54 -0400

    add feature: click button to generate 10 random dies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import { useState } from 'react';
 import './App.css'
 import Die from './components/Die'
 
 function App() {
 
+  // generate an array of 10 random numbers between 1-6 inclusive.
+  // { value: <random number>, isHeld: false }
+  function allNewDice() {
+    let diceArr = [];
+    for (let i = 0; i < 10; i++) {
+      let randomNum = Math.floor(Math.random()*6+1)
+      diceArr.push(randomNum)
+    }
+    return diceArr;
+  }
+  const [dice, setDice] = useState(allNewDice())
+  const diceElements = dice.map(die => <Die value = {die} />)
 
-
-
-  const num = Math.floor(Math.random()*6+1) 
+  function rollDice() {
+    setDice(allNewDice())
+  }
+ 
   return (
     <div>
       <main>
         <div className="dice-container">
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
-          <Die value = {num} />
+          {diceElements}
         </div>
+        <button className='roll-btn' onClick={rollDice}>Roll</button>
       </main>
     </div>
   );
